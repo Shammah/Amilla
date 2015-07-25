18dec4de0235ece94f029d7dbe0673f07df334ac
Shammah <contact@protoman.net>
2015-07-25 21:45:00 +0200

    Add Draw support

--- Source/Chip8/Emulator.cpp
+++ Source/Chip8/Emulator.cpp
@@ -281,7 +281,21 @@ void Emulator::Rand()
 
 void Emulator::Draw()
 {
-    throw exception("Not implemented yet!");
+    /**
+     * Draws a sprite at coordinate(VX, VY) that has a width of 8 pixels
+     * and a height of N pixels.Each row of 8 pixels is read as bit - coded
+     * (with the most significant bit of each byte displayed on the left)
+     * starting from memory location I; I value doesn’t change after the
+     * execution of this instruction.As described above, VF is set to 1 if
+     * any screen pixels are flipped from set to unset when the sprite is drawn,
+     * and to 0 if that doesn’t happen.
+     */
+    GET_VxVy();
+    auto sprite = _storage.RAM.begin();
+    auto screen = _display.Pixels.begin();
+    std::advance(sprite, _state.I);
+    std::advance(screen, Vx + _display.WIDTH * Vy);
+    std::copy_n(screen, _opcode.n, sprite);
 }
 
 void Emulator::Key()
