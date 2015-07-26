6f90684363f6323793ad781beda8399853d05940
Shammah <contact@protoman.net>
2015-07-26 20:09:11 +0200

    Add DI

--- Source/Chip8/Main.cpp
+++ Source/Chip8/Main.cpp
@@ -1,15 +1,20 @@
 #include <memory>
+#include "boost/di.hpp"
 #include "Emulator.hpp"
 #include "EmulatorSFML.hpp"
 using namespace Chip8;
+namespace di = boost::di;
 
 int main()
 {
-    auto chip8 = std::make_shared<Emulator>();
-    chip8->LoadFromFile("Roms/Chip-8 Demos/Maze [David Winter, 199x].ch8");
+    auto injector = di::make_injector(
+        di::bind<IChip8Emulator, Emulator>(),
+        di::bind<IChip8EmulatorShell<sf::Keyboard::Key>, EmulatorSFML>()
+    );
 
-    EmulatorSFML shell(std::static_pointer_cast<IChip8Emulator>(chip8));
-    shell.Start();
+    auto shell = injector.create<std::unique_ptr<IChip8EmulatorShell<sf::Keyboard::Key>>>();
+    shell->GetEmulator()->LoadFromFile("Roms/Chip-8 Demos/Maze [David Winter, 199x].ch8");
+    shell->Start();
 
     return 0;
 }
