2ac600a296422d235e2e07756be8000148f32c22
Shammah <contact@protoman.net>
2016-08-28 23:46:17 +0200

    Fix encoding issue

--- src/Chip8/Emulator.cpp
+++ src/Chip8/Emulator.cpp
@@ -309,10 +309,10 @@ void Emulator::Draw()
      * Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels
      * and a height of N pixels. Each row of 8 pixels is read as bit - coded
      * (with the most significant bit of each byte displayed on the left)
-     * starting from memory location I; I value doesn’t change after the
-     * execution of this instruction.As described above, VF is set to 1 if
+     * starting from memory location I; I value doesn't change after the
+     * execution of this instruction. As described above, VF is set to 1 if
      * any screen pixels are flipped from set to unset when the sprite is drawn,
-     * and to 0 if that doesn’t happen.
+     * and to 0 if that doesn't happen.
      */
     GET_VxVy();
     GET_VF();
