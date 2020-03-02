1f868b32ec65218190f60f05ff497ebe2b5a1dc6
Roy Stoof <contact@protoman.net>
2020-03-02 20:09:36 +0100

    Update keymapping

--- Chip8/Chip8.MonoGame/Chip8.cs
+++ Chip8/Chip8.MonoGame/Chip8.cs
@@ -31,22 +31,22 @@ namespace Amilla.Chip8.MonoGame
 
             keyMapping = new Dictionary<Keys, int>()
             {
-                { Keys.D0, 0x0 },
+                { Keys.X, 0x0 },
                 { Keys.D1, 0x1 },
                 { Keys.D2, 0x2 },
                 { Keys.D3, 0x3 },
-                { Keys.D4, 0x4 },
-                { Keys.D5, 0x5 },
-                { Keys.D6, 0x6 },
-                { Keys.D7, 0x7 },
-                { Keys.D8, 0x8 },
-                { Keys.D9, 0x9 },
-                { Keys.A, 0xA },
-                { Keys.B, 0xB },
-                { Keys.C, 0xC },
-                { Keys.D, 0xD },
-                { Keys.E, 0xE },
-                { Keys.F, 0xF }
+                { Keys.Q, 0x4 },
+                { Keys.W, 0x5 },
+                { Keys.E, 0x6 },
+                { Keys.A, 0x7 },
+                { Keys.S, 0x8 },
+                { Keys.D, 0x9 },
+                { Keys.Z, 0xA },
+                { Keys.C, 0xB },
+                { Keys.D4, 0xC },
+                { Keys.R, 0xD },
+                { Keys.F, 0xE },
+                { Keys.V, 0xF }
             };
         }
 
